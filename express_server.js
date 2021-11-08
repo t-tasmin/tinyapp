8acffa8091d2844c30ebdaa1dd1621d8cad40685
Tania Tasmin <taniat.kaau@email.com>
2021-11-08 18:15:40 -0500

    Modify the code to submit form data using POST request

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true}));
+
+
 //EJS is set as express's templating engine/view engine
 app.set("view engine", "ejs");
 
@@ -15,15 +18,20 @@ const urlDatabase = {
 //This function is called whenever a request is made to our server application.
 // a function which handles requests and sends response
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
-
 
 // If we type http://localhost:8080/urls/b2xVn2 as url then req.params.shortURL=b2xVn2
 // get requests with : should be placed at the end 
@@ -38,3 +46,13 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString(length) {
+  const chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+var rString = generateRandomString(6);
+console.log(rString);
