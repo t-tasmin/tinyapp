af70ef06dafed9b0bce6e0c55e345862632f07a2
Tania Tasmin <taniat.kaau@email.com>
2021-11-08 14:24:54 -0500

    Set ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//EJS is set as express's templating engine/view engine
+app.set("view engine", "ejs");
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
