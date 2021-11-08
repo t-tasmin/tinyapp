9e942854ae784543a9ba3a14b5aa8cee7039cdc7
Tania Tasmin <taniat.kaau@email.com>
2021-11-08 15:21:26 -0500

    Send the data to ejs template urls_index for displaying

--- express_server.js
+++ express_server.js
@@ -18,13 +18,16 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);   //res.send will also work
-});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
