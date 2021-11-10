fab934ee06d3d26f36325408a3a45bfa446f4e77
Tania Tasmin <taniat.kaau@email.com>
2021-11-10 00:07:44 -0500

    Add a POST route that clears cookies after logout and redirects to  /urls

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-//cookie-parser serves as Express middleware that helps us read the values from the cookie. 
+//cookie-parser serves as Express middleware that helps us read the values from the cookie.
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
@@ -27,10 +27,10 @@ const urlDatabase = {
 //************************************GET ROUTES ***********************************/
 //**********************************************************************************/
 app.get("/urls", (req, res) => {
-  
+  console.log(req.cookies["username"]);
   let templateVars = {
-   urls: urlDatabase,
-   username:req.cookies["username"]};///Add an endpoint to handle a POST to /login 
+    urls: urlDatabase,
+    username:req.cookies["username"]};
   
   res.render("urls_index", templateVars); // render means it will create an ejs template and convert to html
 });
@@ -42,16 +42,15 @@ app.get("/urls/new", (req, res) => {
 //req.params is an object {shortURL:the number}
 // If we type localhost:8080/urls/b2xVn2, then req.params.shortURL=b2xVn2
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username:req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  if (req.params.shortURL in urlDatabase)
-  {res.redirect(longURL);}
-  else
-  {
+  if (req.params.shortURL in urlDatabase) {
+    res.redirect(longURL);
+  } else {
     res.send("This URL is not found");
   }
 });
@@ -80,21 +79,26 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // Add a POST route, it triggers when the Edit button of /urls is pressed, it redirects to that particular url
 app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;    
+  const shortURL = req.params.shortURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-// Add a POST route, it triggers when the longURL is entered for updating urlDatabase 
+// Add a POST route, it triggers when the longURL is entered for updating urlDatabase
 app.post("/urls/:shortURL/update", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
 
-
-// Add a POST route, it triggers when the login button is pressed 
+// Add a POST route, it triggers when the login button is pressed
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  res.cookie("username", req.body.name);
+  res.redirect("/urls");
+});
+
+// Add a POST route, it triggers when the logout button is pressed
+app.post("/logout", (req, res) => {
+  res.clearCookie("username", req.body.username);
   res.redirect("/urls");
 });
 
@@ -106,9 +110,9 @@ app.listen(PORT, () => {
 //********************************************************************************//
 //Generate a random alpha-numeric string
 function generateRandomString(length) {
-  const chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var result = '';
-  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 }
 
