ab67c23fdd047fada2674c3f88438f9c43b8a666
Tania Tasmin <taniat.kaau@email.com>
2021-11-11 21:12:56 -0500

    Include two functions and export them

--- helpers.js
+++ helpers.js
@@ -1,23 +1,23 @@
- function getUserByEmail (email, obj) {
+function getUserByEmail(email, obj) {
   for (let key in obj) {
     if (obj[key].email === email)
       return key;
   }
 
   return null;
-};
-
-
-function urlsForUser(urlDatabase ,id){
-  let A= {};
-for (let key in urlDatabase){
-  if (urlDatabase[key].userID === id){
-    A[key]=urlDatabase[key].longURL;
-    
-  }
 }
-return A;
-};
+
+
+function urlsForUser(urlDatabase ,id) {
+  let A = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      A[key] = urlDatabase[key].longURL;
+    
+    }
+  }
+  return A;
+}
 
 
 module.exports = { getUserByEmail, urlsForUser };
